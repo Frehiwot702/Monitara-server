624abd397b5bdcd693de995a5d1a776cede15da1
Frehiwot702 <frehiwot.tewodros112@gmail.com>
2026-07-01 17:08:14 +0300

    feat: configure Firebase initialization using environment variables

--- firebase.js
+++ firebase.js
@@ -1,6 +1,14 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./serviceAccountKey.json");
+require("dotenv").config();
+
+admin.initializeApp({
+    credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
+});
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
