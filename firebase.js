c1ff9f5aa5fcbad8bb1b1c1ffdb3f4117dbf9ecd
Frehiwot702 <frehiwot.tewodros112@gmail.com>
2026-07-01 18:54:47 +0300

    feat: refactor Firebase initialization to use environment variables directly

--- firebase.js
+++ firebase.js
@@ -1,19 +1,21 @@
 const admin = require("firebase-admin");
 
-require("dotenv").config();
+const {
+  FIREBASE_PROJECT_ID,
+  FIREBASE_CLIENT_EMAIL,
+  FIREBASE_PRIVATE_KEY,
+} = process.env;
+
+if (!FIREBASE_PROJECT_ID || !FIREBASE_CLIENT_EMAIL || !FIREBASE_PRIVATE_KEY) {
+  throw new Error("Missing Firebase environment variables");
+}
 
 admin.initializeApp({
-    credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  credential: admin.credential.cert({
+    projectId: FIREBASE_PROJECT_ID,
+    clientEmail: FIREBASE_CLIENT_EMAIL,
+    privateKey: FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
   }),
 });
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-const db = admin.firestore();
-
-module.exports = db;
+module.exports = admin.firestore();
